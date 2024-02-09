0fe8cef234b741571f27fb7dce8e5bd86e88e8f4
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2024-02-09 21:37:05 +0600

    feat: Added testing for test class

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,44 +1,122 @@
-// Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import lodash from 'lodash';
+import {
+  BankAccount,
+  InsufficientFundsError,
+  SynchronizationFailedError,
+  TransferFailedError,
+  getBankAccount,
+} from '.';
 
 describe('BankAccount', () => {
+  let initialBalance = 0;
+  let transferAmount = 0;
+  let account = getBankAccount(initialBalance);
+  let recipientAccount = getBankAccount(initialBalance);
+
+  beforeEach(() => {
+    initialBalance = 100;
+    transferAmount = 100;
+    account = getBankAccount(initialBalance);
+    jest.clearAllMocks();
+  });
+
   test('should create account with initial balance', () => {
-    // Write your test here
+    expect(account).toBeInstanceOf(BankAccount);
+    expect(account.getBalance()).toEqual(initialBalance);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    const withdrawAmount = 150;
+
+    expect(() => account.withdraw(withdrawAmount)).toThrow(
+      InsufficientFundsError,
+    );
+
+    try {
+      account.withdraw(withdrawAmount);
+    } catch (error) {
+      expect(error).toBeInstanceOf(InsufficientFundsError);
+    }
   });
 
   test('should throw error when transferring more than balance', () => {
-    // Write your test here
+    transferAmount = 150;
+
+    expect(() => account.transfer(transferAmount, recipientAccount)).toThrow(
+      InsufficientFundsError,
+    );
+
+    try {
+      account.transfer(transferAmount, recipientAccount);
+    } catch (error) {
+      expect(error).toBeInstanceOf(InsufficientFundsError);
+    }
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    recipientAccount = account;
+
+    expect(() => account.transfer(transferAmount, recipientAccount)).toThrow(
+      TransferFailedError,
+    );
+
+    try {
+      account.transfer(transferAmount, recipientAccount);
+    } catch (error) {
+      expect(error).toBeInstanceOf(TransferFailedError);
+    }
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    const depositAmount = 100;
+    account.deposit(depositAmount);
+
+    expect(account.getBalance()).toEqual(initialBalance + depositAmount);
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    const withdrawAmount = 50;
+    account.withdraw(withdrawAmount);
+
+    expect(account.getBalance()).toEqual(initialBalance - withdrawAmount);
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    account.transfer(transferAmount, recipientAccount);
+
+    expect(account.getBalance()).toEqual(initialBalance - transferAmount);
+    expect(recipientAccount.getBalance()).toEqual(
+      initialBalance + transferAmount,
+    );
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
-    // Write your tests here
+    const fetchBalance = 75;
+    jest
+      .spyOn(lodash, 'random')
+      .mockReturnValueOnce(fetchBalance)
+      .mockReturnValueOnce(1);
+
+    const result = await account.fetchBalance();
+    expect(result).toEqual(fetchBalance);
   });
 
   test('should set new balance if fetchBalance returned number', async () => {
-    // Write your tests here
+    const balance = 75;
+    jest.spyOn(account, 'fetchBalance').mockResolvedValueOnce(balance);
+
+    await account.synchronizeBalance();
+
+    expect(account.getBalance()).toEqual(balance);
   });
 
   test('should throw SynchronizationFailedError if fetchBalance returned null', async () => {
-    // Write your tests here
+    jest.spyOn(account, 'fetchBalance').mockResolvedValueOnce(null);
+
+    try {
+      await account.synchronizeBalance();
+    } catch (error) {
+      expect(error).toBeInstanceOf(SynchronizationFailedError);
+    }
   });
 });
